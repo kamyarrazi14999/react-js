0c13edbf737d4a623a4f2a2112d2d40e1c6b0dbf
kamyarrazi14999 <kami6535@gmail.com>
2025-09-08 13:37:05 +0330

    new props chiedren

--- kamyarrazi/src/App.jsx
+++ kamyarrazi/src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css';
-import { Header } from './compoments/layout/Header';
-// import { Button } from '/components/button/button';
 
 function App(){
 
   return (
     <>
-      <Header
-        ptn_login_text ="دکمه ورودی"
-        ptn_register_text = 'دکمه ثبت نام '
-
-      />
+      
     </>
     
   );
