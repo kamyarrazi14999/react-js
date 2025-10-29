069b4daa8fa6ceb2b70d4819aa6f1fe3033e8338
kamyarrazi14999 <kami6535@gmail.com>
2025-10-29 06:20:36 +0330

    Refactor App component to implement login state management and remove unused code

--- kamyarrazi/src/App.jsx
+++ kamyarrazi/src/App.jsx
@@ -3,26 +3,25 @@ import "./App.css";
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [textinput, setTextInput] = useState('');
-  const hendleClick = () => {
-    setCount(count + 1);
+  const [islogin, setIslogin] = useState(false);
+
     
+  
+  if (islogin == true) {
+    return (
+      <>
+        <h1>کاربر لاگین هست</h1>
+    
+      </>
+    );
+   } else {
+      return (
+        <>
+         <h1>کاربر لاگین نیست</h1>
+        </>
+      )
+    }
   }
 
-  const hendelchang = (event)=> {
-    setTextInput(event.target.value);
-  }
-  return (
-    <>
-      <h2>{count}</h2>
-      <h5> {textinput} </h5>
-      <div className="parent">
-        <button onClick={hendleClick}> کلیک کنید </button> 
-        <input onChange={hendelchang}  placeholder="نام خود را وارد کنید" type="text" />
-        </div>
-    </>
-  );
-}
 
 export default App;
